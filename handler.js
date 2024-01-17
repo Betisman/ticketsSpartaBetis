d1052fba1936a578cc4d5da72b1f093bec8ea2a9
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2024-01-17 11:27:42 +0100

    feat: log messages

--- handler.js
+++ handler.js
@@ -2,7 +2,16 @@
 const TelegramBot = require('node-telegram-bot-api');
 const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium');
+const winston = require('winston');
 require('dotenv').config();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [new winston.transports.Console()],
+});
+
+logger.info('Starting the application');
 
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_CHAT_ID;
@@ -12,17 +21,23 @@ const pinAbonado = process.env.PIN_ABONADO;
 const dni = process.env.DNI;
 
 const searchBetisWeb = async () => {
+  logger.info('Starting the searchBetisWeb function');
   const browser = await puppeteer.launch({
     headless: chromium.headless,
     slowMo: 100,
     executablePath: await chromium.executablePath(),
     ignoreHTTPSErrors: true,
     defaultViewport: chromium.defaultViewport,
-    args:[...chromium.args, '--hide-scrollbars', '--disable-web-security']
+    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security']
   });
+
+  logger.info('Browser launched');
+
   const page = await browser.newPage();
 
   await page.goto("https://abonados.realbetisbalompie.es/index.php/es-es/");
+
+  logger.info('Page loaded');
 
   // type username in #id-abonado
   await page.type('#id-abonado', idAbonado);
@@ -31,8 +46,12 @@ const searchBetisWeb = async () => {
 
   await page.click('#login-abonado-rbb input[type="submit"]');
 
+  logger.info('Login submitted, let\'s wait for 3 seconds (max) or until the page loads');
+
   // wait for 3 seconds or until the page loads
   await new Promise(r => setTimeout(r, 3000));
+
+  logger.info('3 seconds (max) passed, let\'s fill the input #dni with a value');
 
   // fill the input #dni with a value
   await page.type('#dni', dni);
@@ -40,11 +59,18 @@ const searchBetisWeb = async () => {
   // click on the button #validar
   await page.click('#validar');
 
+  logger.info('Validar button clicked, let\'s wait for 3 seconds (max) or until the page loads');
+
   // wait for 3 seconds or until the page loads
   await new Promise(r => setTimeout(r, 3000));
 
   // click on the link a[href="/index.php/es-es/soliciutd-banderas"]
   await page.click('a[href="/index.php/es-es/solicitud-banderas"]');
+
+  logger.info('Solicitud banderas link clicked, let\'s wait for 3 seconds (max) or until the page loads');
+
+  // wait for 3 seconds or until the page loads
+  await new Promise(r => setTimeout(r, 3000));
 
   // find the text "No se ha encontrado ningun evento activo"
   const text = await page.evaluate(() => document.querySelector('body').innerText);
@@ -52,29 +78,40 @@ const searchBetisWeb = async () => {
   const isEventActive = !text.includes('No se ha encontrado ningun evento activo');
   const isZagrebFound = text.includes('Zagreb');
 
+  logger.info(`isEventActive: ${isEventActive}`);
+  logger.info(`isZagrebFound: ${isZagrebFound}`);
+
   await browser.close();
+
+  logger.info('Browser closed');
 
   return { isEventActive, isZagrebFound };
 }
 
 module.exports.check = async (event) => {
-  if (!botToken) {
-    throw new Error('Missing Telegram bot token')
-  }
-
-  if (!chatId) {
-    throw new Error('Missing Telegram chat ID')
-  }
-
-  const bot = new TelegramBot(botToken);
-  
   try {
+    logger.info('Starting the check function');
+
+    if (!botToken) {
+      throw new Error('Missing Telegram bot token')
+    }
+
+    if (!chatId) {
+      throw new Error('Missing Telegram chat ID')
+    }
+
+    const bot = new TelegramBot(botToken);
+
     const { isEventActive, isZagrebFound } = await searchBetisWeb();
+
+    logger.info(`check:isEventActive: ${isEventActive}`);
+    logger.info(`check:isZagrebFound: ${isZagrebFound}`);
+
 
     let message = '';
     if (isEventActive || isZagrebFound) {
       message = `Check the website, there is an event active or Zagreb was found: ${JSON.stringify({ isEventActive, isZagrebFound })}`;
-      console.log(message);
+      logger.info(message);
 
       // Send a message through Telegram
       bot.sendMessage(chatId, message);
@@ -85,7 +122,7 @@ module.exports.check = async (event) => {
       };
     } else {
       message = `:( There is no event active and Zagreb was not found: ${JSON.stringify({ isEventActive, isZagrebFound })}`;
-      console.log(message);
+      logger.info(message);
       bot.sendMessage(chatId, message);
 
       return {
@@ -94,7 +131,7 @@ module.exports.check = async (event) => {
       };
     }
   } catch (error) {
-    console.error('Error making the request:', error);
+    logger.error('Error making the request:', error);
 
     return {
       statusCode: 500,
