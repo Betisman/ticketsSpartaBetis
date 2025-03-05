30f5daee106f8e2a7beaa0f6a00386dabf5bfb54
Carlos Jimenez <betisman@gmail.com>
2025-03-05 19:07:20 +0100

    refactor: configure browser

--- handler.js
+++ handler.js
@@ -34,7 +34,16 @@ module.exports.check = async (event) => {
     let browser = null;
 
     try {
-        browser = await chromium.launch({ headless: true });
+        browser = await chromium.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-gpu',
+                '--disable-dev-shm-usage'
+            ],
+            headless: true
+        });
+
         const context = await browser.newContext({
             userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         });
